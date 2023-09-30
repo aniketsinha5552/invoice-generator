41b67d02010ca77d2c48873f5bbc2d4b43bbe3e1
aniketsinha5552 <104712880+aniketsinha5552@users.noreply.github.com>
2023-09-30 22:05:31 +0530

    Routing and Invoice listing page

--- src/App.js
+++ src/App.js
@@ -3,14 +3,22 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Container from 'react-bootstrap/Container';
 import InvoiceForm from './components/InvoiceForm';
+import InvoiceList from './components/InvoiceList';
+import { Routes,Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
   return (
     <div className="App d-flex flex-column align-items-center justify-content-center w-100">
-      <Container>
-        <InvoiceForm/>
-      </Container>
+      <Routes>
+        <Route path='/' element={<InvoiceList/>} />
+        <Route path='/create' 
+        element={
+          <Container>
+            <InvoiceForm/>
+          </Container>
+        } />
+      </Routes>      
     </div>
   );
 }}
